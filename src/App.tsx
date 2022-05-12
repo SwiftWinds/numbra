64e96a09ec5348ccf5f457891219b2a05e38e542
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-05-11 22:59:50 -0700

    Add basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type { Component } from "solid-js";
+import { createSignal } from "solid-js";
+import { sqrt } from "mathjs";
 
 const App: Component = () => {
+  const [body, setBody] = createSignal("");
+  const [answers, setAnswers] = createSignal([]);
+
+  const handleChange = (e: KeyboardEvent) => {
+    const val = e.target.value;
+    setBody(val);
+    console.log(sqrt(3));
+    console.log(val.split("\n").map(Number));
+    setAnswers(val.split("\n").map(Number).map(n => sqrt(n)));
+    console.log(answers());
+  };
+
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="textarea-container">
+        <textarea onKeyUp={handleChange} value={body()} />
+        <div id="copy">{body}</div>
+        <div id="cut">cut</div>
+        <div id="paste">paste</div>
+      </div>
+      <style jsx>
+        {`
+          div#textarea-container {
+            position: relative;
+            float: left;
+          }
+
+          div#copy {
+            font-size: 1.5rem;
+            position: absolute;
+            right: 5px;
+            top: 0;
+          }
+
+          div#cut {
+            font-size: 1.5rem;
+            position: absolute;
+            right: 5px;
+            top: 25px;
+          }
+
+          div#paste {
+            font-size: 1.5rem;
+            position: absolute;
+            right: 5px;
+            top: 50px;
+          }
+
+          textarea {
+            border: none;
+            background-color: #212226;
+            resize: none;
+            outline: none;
+            width: 99.8vw;
+            height: 99.03vh;
+            font-size: 1.5rem;
+          }
+
+          * {
+            margin: 0;
+            padding: 0;
+            background-color: #212226;
+            color: #fff;
+          }
+        `}
+      </style>
+    </>
   );
 };
 
